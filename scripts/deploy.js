2388e70d806a267bc6ff47a4d64b8f3925f43999
Zhang <Zhang747326@outlook.com>
2026-04-01 00:58:58 +0800

    呃

---
+++ scripts/deploy.js
@@ -0,0 +1,7 @@
+// scripts/deploy.js
+import { execSync } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const distPath = path.resolve(__dirname, "../dist");
